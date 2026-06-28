51c3c51785eb073954d7ec0109958ae385e40b70
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2026-06-28 03:06:56 -0500

    Update configure_ios.sh

--- runtime/configure_ios.sh
+++ runtime/configure_ios.sh
@@ -12,6 +12,6 @@ mkdir -p $BUILD_DIR
 cd $BUILD_DIR
 
 echo "Configuring for $PLATFORM..."
-cmake -S ../.. -B . -G Xcode -DCMAKE_TOOLCHAIN_FILE=../../ios.toolchain.cmake -DPLATFORM=OS64 -DENABLE_ARC=FALSE
+cmake -S ../.. -B . -G Xcode -DCMAKE_TOOLCHAIN_FILE=../../cmake/ios.toolchain.cmake -DPLATFORM=OS64 -DENABLE_ARC=FALSE
 
 echo "Complete! Xcode project is located in $BUILD_DIR"
